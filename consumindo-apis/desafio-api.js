0454a5dc67670af0d9a7a30d19899de7b8559b92
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-04-09 21:59:33 -0300

    Consumindo APIs Código Desafio

---
+++ consumindo-apis/desafio-api.js
@@ -0,0 +1,19 @@
+function buscarIdade() {
+    const nome = document.getElementById('nome').value;
+    fetch(`https://api.agify.io?name=${nome}`)
+        .then(r => r.json())
+        .then(d => {
+            document.getElementById('resultado').innerText =
+                `O nome ${d.name} tem em média ${d.age} anos.`;
+        });
+}
+
+Promise.all([
+    fetch('https://api.agify.io?name=ana').then(r => r.json()),
+    fetch('https://api.genderize.io?name=ana').then(r => r.json()),
+    fetch('https://api.nationalize.io/?name=ana').then(r => r.json())
+]).then(([idade, genero, nacionalidade]) => {
+    console.log("Idade:", idade.age);
+    console.log("Gênero:", genero.gender);
+    console.log("Nacionalidade:", nacionalidade.country);
+});
