fd088956ae0fd97aeea71d392be052ded7381b3f
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-04-11 18:08:55 -0300

    tratamento de erros

--- desafio-via-cep-api/index.js
+++ desafio-via-cep-api/index.js
@@ -1,7 +1,9 @@
+let cep = document.querySelector('#cep');
 let bairro = document.querySelector('#bairro');
 let rua = document.querySelector('#logradouro');
 let cidade = document.querySelector('#localidade');
 let uf = document.querySelector('#uf');
+
 
 const preencherForm = (endereco) => {
     rua.value = endereco.logradouro;
@@ -10,14 +12,25 @@ const preencherForm = (endereco) => {
     uf.value = endereco.uf;
 }
 
+
 const buscarCep = async () => {
-    const cep = document.querySelector('#cep').value;
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
 
     const dados = await fetch(url);
     const endereco = await dados.json();
+
+    /* verifica se possui a propriedade erro = true */
+    const hasError = endereco.hasOwnProperty('erro');
+
+    if (hasError) {
+        alert('O CEP informado é inválido! Por favor, informe um CEP válido.');
+        cep.value = '';
+        return;
+    }
+
     preencherForm(endereco);
-    console.log(endereco);
+
 }
 
 document.querySelector('#cep')
