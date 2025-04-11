7c93b3620cc325179d9d56fddcbece3e815f2844
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-04-10 23:28:51 -0300

    consultando apis externas usando promise.all

--- consumindo-apis/desafio-api.js
+++ consumindo-apis/desafio-api.js
@@ -11,9 +11,9 @@ function buscarIdade() {
 Promise.all([
     fetch('https://api.agify.io?name=ana').then(r => r.json()),
     fetch('https://api.genderize.io?name=ana').then(r => r.json()),
-    fetch('https://api.nationalize.io/?name=ana').then(r => r.json())
+    fetch('https://api.nationalize.io/?name=robson').then(r => r.json())
 ]).then(([idade, genero, nacionalidade]) => {
     console.log("Idade:", idade.age);
     console.log("Gênero:", genero.gender);
-    console.log("Nacionalidade:", nacionalidade.country);
+    console.log("Nacionalidade:", nacionalidade.country[0].country_id);
 });
