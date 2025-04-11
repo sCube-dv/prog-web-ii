5d36f335dd8be245d95cd1042d79903d0e60bc61
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-04-11 18:18:50 -0300

    verificação do cep informado.

--- desafio-via-cep-api/index.js
+++ desafio-via-cep-api/index.js
@@ -4,6 +4,8 @@ let rua = document.querySelector('#logradouro');
 let cidade = document.querySelector('#localidade');
 let uf = document.querySelector('#uf');
 
+/* funcao que valida se o cep tem 8 digitos (expressão regular) */
+const validaCep = (cep) => /^[0-9]{8}$/.test(cep);
 
 const preencherForm = (endereco) => {
     rua.value = endereco.logradouro;
@@ -16,20 +18,25 @@ const preencherForm = (endereco) => {
 const buscarCep = async () => {
     const url = `https://viacep.com.br/ws/${cep.value}/json/`;
 
+    if (validaCep(cep.value)) {
 
-    const dados = await fetch(url);
-    const endereco = await dados.json();
+        const dados = await fetch(url);
+        const endereco = await dados.json();
 
-    /* verifica se possui a propriedade erro = true */
-    const hasError = endereco.hasOwnProperty('erro');
+        /* verifica se possui a propriedade erro = true */
+        const hasError = endereco.hasOwnProperty('erro');
+        if (hasError) {
+            alert('O CEP informado é inválido! Por favor, informe um CEP válido.');
+            cep.value = '';
+            return;
+        }
 
-    if (hasError) {
-        alert('O CEP informado é inválido! Por favor, informe um CEP válido.');
-        cep.value = '';
-        return;
+        preencherForm(endereco);
+
     }
 
-    preencherForm(endereco);
+    alert('O CEP informado deve possuir 8 dígitos.');
+    cep.value = '';
 
 }
 
