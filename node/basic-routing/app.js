98e033cfb4ede3fc361a2dfb45fe5598d1fc2325
sCube-dv <santos.samuel1@acad.ifma.edu.br>
2025-05-13 00:45:10 -0300

    http basics methods implementeds

--- node/basic-routing/app.js
+++ node/basic-routing/app.js
@@ -11,6 +11,19 @@ app.get('/', (req, res) => {
     res.send('GET - Response sended!');
 });
 
+app.post('/', (req, res) => {
+    res.send('Got a POST request');
+});
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user');
+});
+
+
 // execting app
 const PORT = 3000;
 app.listen(PORT, () => {
